32525f8cd7172718f1bd7387542c071d35653b99
APIVerve Team <hello@apiverve.com>
2025-02-03 15:33:49 -0600

    Minor Version Update

---
+++ CountryFlagsQueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class CountryFlagsQueryOptions {
+public string country { get; set; }
+public string format { get; set; }
+public string shape { get; set; }
+}
+
+}
